d04e663fc96b6d2ce366773f0f2fafae8bb84735
Jon Aldekoa <jaldekoa@gmail.com>
2024-09-25 19:35:30 +0200

    Change sleep time in test

--- fiscaldataapi/tests/test_fiscaldata.py
+++ fiscaldataapi/tests/test_fiscaldata.py
@@ -8,6 +8,6 @@ db = list(__endpoints.keys())
 
 @pytest.mark.parametrize("database", db)
 def test_get_fiscaldata(database):
-    time.sleep(0.5)
+    time.sleep(2)
     df = get_fiscaldata(database)
     assert (isinstance(df, pd.DataFrame) and not df.empty)
