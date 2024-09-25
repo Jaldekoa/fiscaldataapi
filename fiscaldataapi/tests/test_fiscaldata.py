0668970c5bb45aa6db01dcdf7cfa393a2fa5d7c2
Jon Aldekoa <jaldekoa@gmail.com>
2024-09-25 19:26:39 +0200

    Add sleep to avoid closing connection with the peer

--- fiscaldataapi/tests/test_fiscaldata.py
+++ fiscaldataapi/tests/test_fiscaldata.py
@@ -1,11 +1,13 @@
 from fiscaldataapi import get_fiscaldata, __endpoints
 import pandas as pd
 import pytest
+import time
 
 db = list(__endpoints.keys())
 
 
 @pytest.mark.parametrize("database", db)
 def test_get_fiscaldata(database):
+    time.sleep(0.5)
     df = get_fiscaldata(database)
     assert (isinstance(df, pd.DataFrame) and not df.empty)
