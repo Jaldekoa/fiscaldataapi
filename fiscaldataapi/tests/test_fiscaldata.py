4601d2b5d18ddec52345fc9a85ec91158ce08eb3
Jon Aldekoa <jaldekoa@gmail.com>
2024-09-26 13:46:14 +0200

    Random choices 10 tests

--- fiscaldataapi/tests/test_fiscaldata.py
+++ fiscaldataapi/tests/test_fiscaldata.py
@@ -1,13 +1,12 @@
 from fiscaldataapi import get_fiscaldata, __endpoints
 import pandas as pd
+import random
 import pytest
-import time
 
-db = list(__endpoints.keys())
+db = random.choices(list(__endpoints.keys()), k=10)
 
 
 @pytest.mark.parametrize("database", db)
 def test_get_fiscaldata(database):
-    time.sleep(2)
     df = get_fiscaldata(database)
     assert (isinstance(df, pd.DataFrame) and not df.empty)
