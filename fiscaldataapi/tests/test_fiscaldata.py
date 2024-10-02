b26cffd16b34ad4ed95840246a873129661363fa
Jon Aldekoa <jaldekoa@gmail.com>
2024-10-02 22:11:12 +0200

    Add 1s sleep time

--- fiscaldataapi/tests/test_fiscaldata.py
+++ fiscaldataapi/tests/test_fiscaldata.py
@@ -8,5 +8,6 @@ db = random.choices(list(__endpoints.keys()), k=5)
 
 @pytest.mark.parametrize("database", db)
 def test_get_fiscaldata(database):
+    time.sleep(1)
     df = get_fiscaldata(database)
     assert (isinstance(df, pd.DataFrame) and not df.empty)
--- fiscaldataapi/tests/test_info_fiscaldata.py
+++ fiscaldataapi/tests/test_info_fiscaldata.py
@@ -1,12 +1,13 @@
 from fiscaldataapi import info_fiscaldata, __endpoints
-import pandas as pd
 import random
 import pytest
+import time
 
 db = random.choices(list(__endpoints.keys()), k=5)
 
 
 @pytest.mark.parametrize("database", db)
 def test_info_fiscaldata(database):
+    time.sleep(1)
     data_format, total_pages, total_count = info_fiscaldata(database)
     assert isinstance(data_format, dict) and isinstance(total_pages, int) and isinstance(total_count, int)
